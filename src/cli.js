c9cf96a4d8982c8bf71078ff31e599177ec2ac1b
FelisbinoJose <246760.jose@unibave.net>
2026-01-04 15:12:58 -0300

    feat: adiciona biblioteca externa ao código

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,53 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
+import { Command } from 'commander';
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const program = new Command();
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    try {
-        if (erro) throw erro
-        const resultado = contaPalavras(texto);
-        criaESalvaArquivo(resultado, endereco);
-        contaPalavras(texto);
-    } catch (erro) {
-        console.log(trataErros(erro));
-    }
-});
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do testo a ser processado')
+    .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo dos resultados')
+    .action((option) => {
+        const { texto, destino } = option;
+
+        if (!texto || !destino) {
+            console.error('erro: favor inserir caminho de origem e destino')
+            program.help();
+            return
+        }
+
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+
+        try {
+            processaArquivo(caminhoTexto, caminhoDestino)
+            console.log('Texto processado com sucesso');
+
+        } catch (erro) {
+            console.log('Ocorreu um erro no processamento:', erro);
+
+        }
+    })
+
+program.parse();
+
+function processaArquivo(texto, destinho) {
+    fs.readFile(texto, 'utf-8', (erro, texto) => {
+        try {
+            if (erro) throw erro
+            const resultado = contaPalavras(texto);
+            criaESalvaArquivo(resultado, destinho);
+        } catch (erro) {
+            trataErros(erro);
+        }
+    });
+}
+
+
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`;
